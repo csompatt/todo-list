661d9661ef186bc6ccd71452c303c9b673476a89
csompatt <csompatt@icloud.com>
2022-06-15 22:12:58 +0200

    Layout and Styling

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,37 @@
 <template>
-  <header id="header">
-    <button @click="editName"><i class="fa-solid fa-pen-to-square"></i></button>
-    <h1>{{ user }}'s to-do list</h1>
+  <header>
+    <button @click="editName"><i class="fa-solid fa-pen-to-square fa-m"></i></button>
+    <h1 class="d-inlineBlock half-marginLeft">{{ user }}'s to-do list</h1>
   </header>
-  <section> <input type="text" v-model="taskInput" @keypress.enter="addTaskToDoList">
+  <section> <input class="w-80 bg-glass" type="text" v-model="taskInput" @keypress.enter="addTaskToDoList"
+      placeholder="Add your task...">
+    <button id="listChangerBtn" class="d-flex-center w-80" @click="changeList">
+      <span :class="[listDisplay ? listSelectorBtnClass : '']">Tasks to-do</span>
+      <span :class="[!listDisplay ? listSelectorBtnClass : '']">Finished Tasks</span></button>
   </section>
-  <section>
-    <h2>Tasks to-do:</h2>
+  <section v-if="listDisplay" class="list-section">
     <ul>
-      <li v-for="(taskToDo, index) in tasksToDo" :key="taskToDo">{{ taskToDo }}
-        <button @click="moveTask(this.tasksToDo, this.finishedTasks, index)"><i class="fa-solid fa-check"></i></button>
-        <button @click="deleteTask(this.tasksToDo, index)"><i class="fa-solid fa-xmark"></i></button>
-
+      <li v-for="(taskToDo, index) in tasksToDo" :key="taskToDo" class="d-flex-center w-80 bg-glass">
+        <span>{{ taskToDo }}</span>
+        <div class="btn-pos half-marginLeft">
+          <button class="btn btn-green" @click="moveTask(this.tasksToDo, this.finishedTasks, index)">
+            <i class="fa-solid fa-check fa-xs"></i></button>
+          <button class="btn half-marginLeft btn-red" @click="deleteTask(this.tasksToDo, index)">
+            <i class="fa-solid fa-xmark fa-xs"></i></button>
+        </div>
       </li>
     </ul>
   </section>
-  <section>
-    <h2>Finished tasks:</h2>
+  <section v-if="!listDisplay" class="list-section">
     <ul>
-      <li v-for="(finishedTask, index) in finishedTasks" :key="finishedTask">{{ finishedTask }}
-        <button @click="moveTask(this.finishedTasks, this.tasksToDo, index)"><i
-            class="fa-solid fa-arrow-rotate-left"></i></button>
-        <button @click="deleteTask(this.finishedTasks, index)"><i class="fa-solid fa-xmark"></i></button>
+      <li v-for="(finishedTask, index) in finishedTasks" :key="finishedTask" class="d-flex-center w-80 bg-glass">
+        <span>{{ finishedTask }}</span>
+        <div class="btn-pos half-marginLeft">
+          <button class="btn btn-blue" @click="moveTask(this.finishedTasks, this.tasksToDo, index)">
+            <i class="fa-solid fa-arrow-rotate-left fa-xs"></i></button>
+          <button class="btn half-marginLeft btn-red" @click="deleteTask(this.finishedTasks, index)">
+            <i class="fa-solid fa-xmark fa-xs"></i></button>
+        </div>
       </li>
     </ul>
   </section>
@@ -35,9 +45,9 @@
 export default {
   name: 'App',
   beforeMount() {
-    let retrievedUserName = JSON.parse(localStorage.getItem('userName'));
-    let retrievedToDoList = JSON.parse(localStorage.getItem('toDoList'));
-    let retrievedFinishedList = JSON.parse(localStorage.getItem('finishedList'));
+    const retrievedUserName = JSON.parse(localStorage.getItem('userName'));
+    const retrievedToDoList = JSON.parse(localStorage.getItem('toDoList'));
+    const retrievedFinishedList = JSON.parse(localStorage.getItem('finishedList'));
 
     retrievedUserName ? this.user = retrievedUserName : this.editName();
     retrievedToDoList ? this.tasksToDo = retrievedToDoList : '';
@@ -48,7 +58,9 @@ export default {
       user: '',
       taskInput: '',
       tasksToDo: [],
-      finishedTasks: []
+      finishedTasks: [],
+      listDisplay: true,
+      listSelectorBtnClass: 'selected-list'
     }
   },
   watch: {
@@ -81,7 +93,9 @@ export default {
       toArray.push(fromArray[index]);
       this.deleteTask(fromArray, index);
     },
-
+    changeList() {
+      this.listDisplay = !this.listDisplay;
+    }
   },
 }
 </script>
